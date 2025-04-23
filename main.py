27417f67e4960c0b546cee66f419a07344e68928
John Hogan <johnaddisonhogan@gmail.com>
2025-04-23 15:50:03 -0400

    Added switch and other logic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import xlwt
 from xlwt import Workbook
 import json
 import scrapy
+import os
 from scrapy.http import TextResponse
 from scrapy.selector import Selector
 
@@ -34,11 +35,10 @@ def load_fields(json_fields):
             fields.append(value)
     return fields
 
-def write_to_sheet(sheet_tab, response_data, fields):
+def write_to_sheet(response_data, sheet_tab, fields):
     for col, field_name in enumerate(fields):
         # worksheet.write(0, col, field_name)
         sheet_tab.write(0, col, field_name)
-
     for row, response in enumerate(fields, start=1):
         for col, field in enumerate(fields):
             xpath = special_xpath.get(field, standard_xpath(field))
@@ -53,6 +53,7 @@ def write_to_sheet(sheet_tab, response_data, fields):
 
 ##################################################### Page Specific functions #####################################################
 
+#Currently unable to test this due to not having a login to Taleo. 
 def lockheed(driver, spreadsheet, webconfig_data):
     all_responses = []
     xpath = webconfig_data['page_elements']
@@ -80,9 +81,13 @@ def lockheed(driver, spreadsheet, webconfig_data):
     write_to_sheet(spreadsheet, all_responses)
 
 def sunayu(driver, spreadsheet, webconfig_data):
-    #do nothing
     all_responses = []
     xpath = webconfig_data['page_elements']
+    driver.get(webconfig_data['URL'])
+    links = driver.find_elements_by_xpath(xpath['hrefs'])
+    print(links)
+    hrefs = [el.get_attribute('href') for el in links]
+    print(hrefs)
     return 0
 
 
@@ -96,22 +101,31 @@ def main():
     all_responses = []
 
     driver = webdriver.Firefox()
-          
-    # Workbook is created
-    wb = Workbook()
-    # sheet = wb.add_sheet('Sheet 1')
-    # with open('website_configs/lockheed.json') as file:
-    #     webconfig_data = json.load(file)
-    # lockheed(driver, sheet, webconfig_data)
-
-    sheet = wb.add_sheet('Sheet 2')
-    with open('website_configs/sunayu.json'):
-        webconfig_data = json.load(file)
-    lockheed(driver, sheet, webconfig_data)
+    workbook = Workbook()
     
+    sheet = wb.add_sheet('Sunayu_Raw')
+    with open('website_configs/sunayu.json') as file:
+        webconfig_data = json.load(file)
+    sunayu(driver, sheet, webconfig_data)
+    
+    for file in os.listdir("website_configs/"):
+        filename = os.fsdecode(file)
+        if filename.endswith(".json"):
+            print("Scraping using file: " + os.path.join("website_configs/", filename))
+            if "lockheed" in filename:
+                continue # lockheed(driver, workbook.add_sheet('lockheed'), json.load(file))
+            elif "sunayu" in filename:
+                sunayu(driver, workbook.add_sheet('sunayu'), json.load(file))
+            else:
+                print(f"Unable to find scraping algorithm for {filename}")
+        else:
+            print("Error decoding file type, issue with json values. Something funny is happening?")
+            continue
+
+
 
     driver.quit()
-    wb.save('artius.xls')
+    wb.save('artius.csv')
 
 if __name__ == '__main__':
     main()
